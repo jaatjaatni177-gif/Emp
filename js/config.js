4ec60ef9abaedaecc1ab21adf247830c696099e7
jaatjaatni177-gif <jaatjaatni177@gmail.com>
2025-08-24 12:50:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // edit this with your deployed Apps Script Web App URL
 window.CONFIG = {
-  API_BASE: "REPLACE_WITH_YOUR_DEPLOYED_APPS_SCRIPT_URL"
+  API_BASE: "https://script.google.com/macros/s/AKfycbwI2IFPXzGdV1jj9xl8aFQ0Cwr7nc3Wq290Kxp2N-IunQuDX_W_PHQzOu_HAr9Nf5RZ2g/exec"
 };
