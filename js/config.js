2f714aad3d914108623b7454bf158cc6ffe9c5cd
jaatjaatni177-gif <jaatjaatni177@gmail.com>
2025-08-24 17:37:50 +0530

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-// edit this with your deployed Apps Script Web App URL
-window.CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbwI2IFPXzGdV1jj9xl8aFQ0Cwr7nc3Wq290Kxp2N-IunQuDX_W_PHQzOu_HAr9Nf5RZ2g/exec"
-};
--- js/sha256.js
+++
@@ -1,7 +0,0 @@
-// simple SHA-256 wrapper using SubtleCrypto
-async function sha256(message) {
-  const enc = new TextEncoder();
-  const data = enc.encode(message);
-  const hash = await crypto.subtle.digest('SHA-256', data);
-  return Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
-}
